cd5f006a4cad3a827e7db63d4718ca895874404b
islomkhudoyberdiev <islomkhudoyberdiev70@gmail.com>
2024-04-17 17:03:23 +0800

    islom

--- src/io/FrontEnd.java
+++ src/io/FrontEnd.java
@@ -3,5 +3,6 @@ package io;
 public class FrontEnd {
     public static void main(String[] args) {
         System.out.printf ("Hello World G'ayrat");
+        System.out.println ("Hello Islom" );
     }
 }
