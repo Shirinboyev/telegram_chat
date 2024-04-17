767bf0c0c2e9db66fac56de56d5bb95ba6a91fc4
Gayrat <gayratshirinboyev6707@gmail.com>
2024-04-17 19:31:53 +0500

    started

--- src/io/FrontEnd.java
+++ src/io/FrontEnd.java
@@ -2,8 +2,6 @@ package io;
 
 public class FrontEnd {
     public static void main(String[] args) {
-        System.out.println ("Hello World G'ayrat");
-        System.out.println ("Hello Islom" );
-        System.out.println("Hello G'ayrat");
+        System.out.println("Davay boshladik Islom");
     }
 }
