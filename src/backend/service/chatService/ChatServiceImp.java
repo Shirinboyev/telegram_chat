5cb54eceb6c11b8af4bab38ecf0ea2e1176e5b27
islomkhudoyberdiev <islomkhudoyberdiev70@gmail.com>
2024-04-19 21:54:38 +0800

    Chat ServiceImp method lari Override qilind

--- src/backend/service/chatService/ChatServiceImp.java
+++ src/backend/service/chatService/ChatServiceImp.java
@@ -1,4 +1,54 @@
 package backend.service.chatService;
 
-public class ChatServiceImp {
+import backend.dto.LoginDto;
+import backend.model.user.User;
+import backend.service.userService.UserService;
+
+import java.util.List;
+
+public class ChatServiceImp implements UserService {
+    @Override
+    public User login(LoginDto login, List<User> users) {
+        return null;
+    }
+
+    @Override
+    public User login(LoginDto login) {
+        return null;
+    }
+
+    @Override
+    public void signUp(User user) {
+
+    }
+
+    @Override
+    public void deleteGroup(String groupId) {
+
+    }
+
+    @Override
+    public User getGroup(String id) {
+        return null;
+    }
+
+    @Override
+    public List<User> getGroupList() {
+        return List.of ( );
+    }
+
+    @Override
+    public boolean createGroup() {
+        return false;
+    }
+
+    @Override
+    public boolean createContact() {
+        return false;
+    }
+
+    @Override
+    public User getContact() {
+        return null;
+    }
 }
