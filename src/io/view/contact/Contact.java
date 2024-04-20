3a66e03cc02fb358a39bbd028e6f5cc77cfa0667
islomkhudoyberdiev <islomkhudoyberdiev70@gmail.com>
2024-04-20 17:03:04 +0800

    chat Servicni  metodlari yaratildi

---
+++ src/io/view/contact/Contact.java
@@ -0,0 +1,20 @@
+package io.view.contact;
+
+import io.FrontEnd;
+import io.utils.Utils;
+
+public class Contact {
+    public static void addContact() {
+        Integer number = Utils.enterInt("Enter number : ");
+
+        if (number.equals(FrontEnd.currentUser.getPhoneNumber())) {
+            System.out.println("It is your number idiot! ");
+            return;
+        }
+
+
+
+
+
+    }
+}
