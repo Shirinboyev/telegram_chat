46e705443a080376648fdad14519ecdc91bc49fa
islomkhudoyberdiev <islomkhudoyberdiev70@gmail.com>
2024-04-20 17:29:02 +0800

    chat Servicni  metodlari yaratildi

--- src/backend/service/chatService/ChatServiceImp.java
+++ src/backend/service/chatService/ChatServiceImp.java
@@ -27,27 +27,43 @@ public class ChatServiceImp implements ChatService {
 
     @Override
     public List<Chat> getChatsOfUser(String userId) {
-        return List.of ( );
+        List<Chat> chatsOfUser = new ArrayList<>();
+        for (Chat chat1 : chat) {
+            if (chat1.getFirstUserId().equals(userId) || chat1.getSecondUserId().equals(userId)) {
+                chatsOfUser.add(chat1);
+            }
+        }
+        return chatsOfUser;
     }
 
     @Override
     public Chat getChatOfUser(String userId) {
+        for (Chat chats : chat) {
+            if (chats.getFirstUserId().equals(userId) || chats.getSecondUserId().equals(userId)) {
+                return chats;
+            }
+        }
         return null;
     }
 
     @Override
     public boolean Exist(String userId) {
+        for (Chat chats : chat) {
+            if (chats.getFirstUserId().equals(userId) || chats.getSecondUserId().equals(userId)) {
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
-    public boolean add(Chat o) {
-        return false;
+    public boolean add(Chat ch) {
+        return chat.add (ch);
     }
 
     @Override
-    public boolean delete(Chat chat) {
-        return false;
+    public boolean delete(Chat chat0) {
+        return chat.remove(chat0);
     }
 
     @Override
@@ -63,6 +79,6 @@ public class ChatServiceImp implements ChatService {
 
     @Override
     public List<Chat> getList() {
-        return List.of ( );
+        return chat;
     }
 }
