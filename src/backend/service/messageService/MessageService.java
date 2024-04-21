ba8f543d65f338e929484c70c93558329cc69829
islomkhudoyberdiev <islomkhudoyberdiev70@gmail.com>
2024-04-21 15:54:20 +0800

    Contac Servis yaratildi

--- src/backend/service/messageService/MessageService.java
+++ src/backend/service/messageService/MessageService.java
@@ -1,4 +1,17 @@
 package backend.service.messageService;
 
-public interface MessageService {
+import backend.model.chat.Chat;
+import backend.model.massages.Massages;
+import backend.service.baseService.BaseService;
+
+import java.util.List;
+
+public interface MessageService extends BaseService<Massages> {
+    List<Massages> getMessagesOfChat(Chat chat);
+
+    List<Massages> getMessagesGroupOrChannel(String groupId);
+
+    int countNotReadMessages(String fromId, String toId);
+
+
 }
